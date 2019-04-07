260e8441888b75484a718eecb06fbce13797a42e
Unknown <stefan.varga98@gmail.com>
2019-04-07 18:58:18 +0200

    weather by hour done

---
+++ Project/Project/HourTemp.cs
@@ -0,0 +1,9 @@
+﻿namespace Project
+{
+    public class HourTemp
+    {
+        public string hour { get; set; }
+        public string temp { get; set; }
+        public string img { get; set; }
+    }
+}
