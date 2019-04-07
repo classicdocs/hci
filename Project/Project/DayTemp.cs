4ad225752341f772ea82f8724971eb1f7939660e
Unknown <stefan.varga98@gmail.com>
2019-04-07 23:42:41 +0200

    weather by day done

---
+++ Project/Project/DayTemp.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project
+{
+    public class DayTemp
+    {
+        public string day { get; set; }
+        public string temp { get; set; }
+        public string date { get; set; }
+        public List<HourTemp> hoursTemp { get; set; }
+        public string img1 { get; set; }
+        public string img2 { get; set; }
+    }
+}
